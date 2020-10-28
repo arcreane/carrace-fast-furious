52cfd865ba909c3bc3b3ef2d46fa1c68c022b786
Léna <lenapancher@gmail.com>
2020-10-28 15:40:19 +0100

    ajout du menu

--- src/Game.java
+++ src/Game.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Game {
 
     public static void main(String[] args) {
-        
+
     }
     /**
      * Default constructor
--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,14 @@
 
 import java.util.*;
 
+import static java.lang.System.out;
+
 /**
  * 
  */
 public class Menu {
+
+    Scanner scanner = new Scanner(System.in);
 
     /**
      * Default constructor
@@ -13,20 +17,43 @@ public class Menu {
     }
 
     /**
-     * 
+     *
+     * @return
      */
-    public void Color () {
+    public String ModeChoice() {
+        try {
+            out.println("Please select the King of car you want :");
+            out.println("- Type 1 for a speeding car but having 30% chance of failing components");
+            out.println("- Type 2 for a slow car but having 50% chance of having a speeding boost");
+            String option = scanner.nextLine();
+            return option;
 
-    };
-
-    /**
-     * 
-     */
-    public void ModeChoice () {
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
+    /**
+     * @return
+     */
+    public String Color() {
+        try {
+            out.println("RED");
+            out.println("BLUE");
+            out.println("GREEN");
+            out.println("YELLOW");
+            out.println("MAGENTA");
+            out.println("CYAN");
+            out.println("WHITE");
+            String option = scanner.nextLine();
+            return option;
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-
+        return "";
+    }
 }
+
